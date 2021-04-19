1949fe47580a6ce1de9b1ac459e27681e6704911
Veselin Georgiev <veselin.georgief@gmail.com>
2021-04-18 22:05:00 -0400

    filled up some info

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import initTilt from "./js/tilt";
 import initSr from "./js/sr";
-import resume from "./assets/resume.pdf";
+import resume from "./assets/Veselin-Georgiev.pdf";
 
 import "./style/main.scss";
 
@@ -27,5 +27,4 @@ function addResume(pdf) {
 initSr();
 initTilt();
 
-// uncomment this if you want to attach your resume
 addResume(resume);
